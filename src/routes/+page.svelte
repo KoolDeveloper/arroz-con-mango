a4abb914b4a99da57cef88bc8f35e8c527d0add8
KoolDeveloper <koolxdev@gmail.com>
2026-09-25 11:40:39 -0500

    Actualizaciones post meeting, se define tematica y estructura

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,31 +5,41 @@
 	import Footer from '$lib/components/ui/Footer.svelte';
 </script>
 
-<div class="bg-linear-to-tl from-[#F58529] via-[#FF0000] to-[#F58529] text-xl">
-	<main class="grid h-screen w-screen grid-cols-32 grid-rows-18 overflow-hidden">
-		<section
-			class="col-span-8 col-start-2 row-span-17"
-		>
-			<img class="h-full w-full rounded-b-md" src="/banner.jpeg" alt="" />
-		</section>
-		<section
-			class="font-cursive col-span-14 col-start-11 row-span-3 rounded-b-md text-white px-4 py-2"
-		>
-			<div class="flex items-center gap-2">
-				<h3 class="font-title">Alejando Piña</h3>
-				<Icon icon={mymountsinai} class="w-12 h-12 text-primary" />
+<main class="overflow-x-hidden overflow-y-scroll">
+	<section class="relative">
+		<div class="grid h-112.5 grid-cols-2 overflow-hidden bg-black shadow-2xl">
+			<div class="relative">
+				<div class="h-full w-full bg-[url('/banner.jpeg')] bg-cover bg-center"></div>
+				<div class="absolute inset-0 bg-linear-to-l from-black via-black/10 to-transparent"></div>
 			</div>
-			<p>
-				Creo en el poder de las conversaciones honestas para transformar vidas. He vivido en carne
-				propia que siempre hay una salida y por eso hoy acompaño a otros a encontrar la suya.
-			</p>
-		</section>
-		<section
-			class="text-md col-span-21 col-start-11 row-span-12 row-start-5 rounded-md text-white p-2"
-		>
-			<CardGallery />
-		</section>
 
-		<Footer />
-	</main>
-</div>
+			<div class="flex flex-col justify-center p-12 text-white">
+				<h1 class="mb-4 text-5xl font-bold">LIBÉRATE DE LA DEPENDENCIA Y REDISEÑA TU FUTURO</h1>
+
+				<p class="mb-8 text-lg text-gray-300">
+					No cambiamos cuando vemos las cosas diferentes, sino cuando nos damos cuenta de que el
+					observador que somos ya no puede sostener la realidad que nos tratamos de inculcar.
+				</p>
+
+				<button class="w-fit rounded-lg bg-[#F58529] px-6 py-3 font-semibold hover:bg-[F58529]/90">
+					Agenda tu cita
+				</button>
+			</div>
+		</div>
+	</section>
+	<section class="font-cursive rounded-b-md px-4 py-2">
+		<div class="flex items-center gap-2">
+			<h3 class="font-title">Alejando Piña</h3>
+			<Icon icon={mymountsinai} class="h-12 w-12 text-primary" />
+		</div>
+		<p>
+			Creo en el poder de las conversaciones honestas para transformar vidas. He vivido en carne
+			propia que siempre hay una salida y por eso hoy acompaño a otros a encontrar la suya.
+		</p>
+	</section>
+	<section class="text-md rounded-md p-2">
+		<CardGallery />
+	</section>
+
+	<Footer />
+</main>
