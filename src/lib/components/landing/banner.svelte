c9fe5d7d85de38b5afdd68ab91b11581e8928b56
KoolDeveloper <koolxdev@gmail.com>
2026-09-26 09:24:14 -0500

    responsive design

---
+++ src/lib/components/landing/banner.svelte
@@ -0,0 +1,25 @@
+<section>
+	<div class="bg-black shadow-2xl grid grid-rows-2 md:grid-rows-1 md:h-112.5 md:grid-cols-2">
+		<div class="relative">
+			<div class="h-full w-full bg-[url('/banner.jpeg')] bg-cover bg-center"></div>
+			<div class="absolute inset-0 bg-linear-to-t md:bg-linear-to-l from-black via-black/10 to-transparent"></div>
+		</div>
+
+		<div class="flex flex-col justify-center items-center px-6 pb-6 md:p-12 text-white">
+			<h1 class="mb-4 text-5xl font-bold">LIBÉRATE DE LA DEPENDENCIA Y REDISEÑA TU FUTURO</h1>
+
+			<p class="mb-8 text-lg text-gray-300">
+				No cambiamos cuando vemos las cosas diferentes, sino cuando nos damos cuenta de que el
+				observador que somos ya no puede sostener la realidad que nos tratamos de inculcar.
+			</p>
+
+			<a
+				class="w-fit rounded-lg bg-primary px-6 py-3 font-semibold transition-colors hover:bg-primary/90"
+				href="https://wa.me/573028023341"
+				target="_blank"
+			>
+				Agenda tu cita
+			</a>
+		</div>
+	</div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,31 +2,12 @@
 	import Icon from '@iconify/svelte';
 	import mymountsinai from '@iconify-icons/arcticons/mymountsinai';
 	import CardGallery from '$lib/components/landing/cardGallery.svelte';
+	import Banner from '$lib/components/landing/banner.svelte';
 	import Footer from '$lib/components/ui/Footer.svelte';
 </script>
 
-<main class="overflow-x-hidden overflow-y-scroll">
-	<section class="relative">
-		<div class="grid h-112.5 grid-cols-2 overflow-hidden bg-black shadow-2xl">
-			<div class="relative">
-				<div class="h-full w-full bg-[url('/banner.jpeg')] bg-cover bg-center"></div>
-				<div class="absolute inset-0 bg-linear-to-l from-black via-black/10 to-transparent"></div>
-			</div>
-
-			<div class="flex flex-col justify-center p-12 text-white">
-				<h1 class="mb-4 text-5xl font-bold">LIBÉRATE DE LA DEPENDENCIA Y REDISEÑA TU FUTURO</h1>
-
-				<p class="mb-8 text-lg text-gray-300">
-					No cambiamos cuando vemos las cosas diferentes, sino cuando nos damos cuenta de que el
-					observador que somos ya no puede sostener la realidad que nos tratamos de inculcar.
-				</p>
-
-				<button class="w-fit rounded-lg bg-[#F58529] px-6 py-3 font-semibold hover:bg-[F58529]/90">
-					Agenda tu cita
-				</button>
-			</div>
-		</div>
-	</section>
+<main class="overflow-x-hidden">
+	<Banner />
 	<section class="font-cursive rounded-b-md px-4 py-2">
 		<div class="flex items-center gap-2">
 			<h3 class="font-title">Alejando Piña</h3>
